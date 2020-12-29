98b266557b1c63d9645b23427d00c64f3582b8da
GabrielSaliba <51325483+GabrielSaliba@users.noreply.github.com>
2020-12-29 20:35:13 -0300

    spring boot initialization

---
+++ src/main/java/com/studyingworkspace/StudyingWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.studyingworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingWorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyingWorkspaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyingworkspace/StudyingWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyingworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingWorkspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
